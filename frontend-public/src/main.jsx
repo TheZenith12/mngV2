b6bddfd43565bceff44dc7d3690230fde8971759
TheZenith12 <zenitht35@gmail.com>
2025-11-10 18:58:27 +0800

    Abouts

--- frontend-public/src/main.jsx
+++ frontend-public/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Details from "./Pages/Details.jsx";
-import About from "./Pages/About.jsx";
 import "./index.css";
 
 import { registerSW } from "virtual:pwa-register";
@@ -20,7 +19,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/details/:id" element={<Details />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
